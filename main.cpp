95b600e5636fda6d10b59c73a5e0242204e50403
yuhua.cheng <vincentcheng929@gmail.com>
2020-12-23 14:53:18 +0800

    Finish retinaface MNN demo!

--- main.cpp
+++ main.cpp
@@ -12,19 +12,27 @@ int main(int, char**) {
     cv::Mat image = cv::imread("/Users/vincent/Documents/Repo/RetinaFace_MNN/test.jpg");
     RetinaFace detector("/Users/vincent/Documents/Repo/RetinaFace_MNN/retinaface.mnn");
 
+    const int max_side = 320;
+    float long_side = std::max(image.cols, image.rows);
+    float scale = max_side/long_side;
+    scale = 1.0f;
+   	cv::Size size = cv::Size(image.cols*scale, image.rows*scale);
+   	cv::Mat image_scale;
+    cv::resize(image, image_scale, size);
+	
     std::vector<BBox> final_bboxes;
-
-    detector.detect(image, final_bboxes);
+    detector.detect(image_scale, final_bboxes);
 
     std::cout << "total faces:" << final_bboxes.size() << std::endl;
     for (BBox& bbox : final_bboxes) {
-    	cv::rectangle(image, cv::Point(bbox.x1, bbox.y1), cv::Point(bbox.x2, bbox.y2), cv::Scalar(255, 0, 0), 2);
+        cv::putText(image, std::to_string(bbox.score), cv::Size(bbox.x1/scale - 5, bbox.y1/scale - 5), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar(0, 255, 255));
+    	cv::rectangle(image, cv::Point(bbox.x1/scale, bbox.y1/scale), cv::Point(bbox.x2/scale, bbox.y2/scale), cv::Scalar(255, 0, 0), 2);
     	for(int i = 0; i < 5; i++) {
-    		cv::Point p(bbox.landmarks[i].x, bbox.landmarks[i].y);
-    		cv::circle(image, p, 2, cv::Scalar(255, 0, 0), 1);
+    		cv::Point p(bbox.landmarks[i].x/scale, bbox.landmarks[i].y/scale);
+    		cv::circle(image, p, 1, cv::Scalar(255, 0, 0), 4);
     	}
     }
-    
+    cv::imwrite("../result.jpg", image);
     cv::imshow("image", image);
     cv::waitKey(0);
 
